2a795534efbde0482bab1b2d1b2ea5b427604123
Elizabeth-Jameson <elizabethjameson03@gmail.com>
2025-04-23 13:45:08 +0100

    Modified the index.js file.

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ const birthdayJob = require('./Birthday-Reminder-main/config/cron')
 
 const app = express()
 
-// Middleware
-app.use(cors())
-app.use(express.json())
-app.use(express.static('public'))
 
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err))
+
+  // Start cron job
+birthdayJob.start()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.static(__dirname, 'public'))
+
+
 
 // Routes
 app.use('/api', userRoutes)
@@ -27,8 +33,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-// Start cron job
-birthdayJob.start()
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
