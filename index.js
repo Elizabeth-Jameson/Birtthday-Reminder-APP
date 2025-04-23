d8a072486b91f113a717ed7cebb9f71cd90c6cdb
Elizabeth-Jameson <elizabethjameson03@gmail.com>
2025-04-23 14:09:40 +0100

    fix: index.js.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on ${PORT}`)
 })
